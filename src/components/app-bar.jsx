dba205ffa787ad0d8e0be90944009d7bba35ead7
Abhimanyu Chakravarty <viperzenator@gmail.com>
2016-12-29 11:43:49 +0530

    Updated failing lint command

---
+++ src/components/app-bar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AppBar = () => <div />;
+
+export default AppBar;
--- src/components/breadcrumb.jsx
+++ src/components/breadcrumb.jsx
@@ -3,4 +3,8 @@ import { ToolbarTitle } from 'material-ui/Toolbar';
 
 const Breadcrumb = ({ path }) => <ToolbarTitle text={ path } style={ { fontFamily: 'roboto' } } />;
 
+Breadcrumb.propTypes = {
+	path: React.PropTypes.string.isRequired,
+};
+
 export default Breadcrumb;
